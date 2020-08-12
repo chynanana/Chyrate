5d321019d8229e970e0cbdb605777e7d395cee78
Chynna <chyanna106@gmail.com>
2020-08-12 17:37:47 +0100

    Selectors and review posters added

--- Reviews.js
+++ Reviews.js
@@ -1,6 +1,6 @@
 const REST_ADDR = "http://localhost:9001/";
 
-function getAllDirectors() {
+function getAllReviews() {
     let request = new XMLHttpRequest();
     request.open('GET', REST_ADDR + "allMovies", true);    
 
@@ -17,27 +17,22 @@ function getAllDirectors() {
         let movieNum = 0;
 
         data.forEach((movie) => {
-            if(movieNum < 5) {
+            console.log('Looking at movie: ' + movieNum);
+
+            if(movieNum < 50) {
                 idTag = '#Movie' + movieNum;  // #Movie0, ...
                 titleContainer = $(idTag + ' .reviewTitle');  // #Movie0 .movie-name
                 titleContainer.html(movie.title); 
-                reviewContainer = $(idTag + '.reviewText');
+                reviewContainer = $(idTag + ' .reviewText');
                 reviewContainer.html(movie.review);
                 img = $(idTag + ' .movie-pic');
-                img.attr('src', movie.img);
-                chyrate =$(idTag + '.chyrate')
-
-
-
-                idTag = '#Movie' + movieNum;
-                nameContainer = $(idTag + ' .director-name');
-                nameContainer.html(director.name);
-                bioContainer = $(idTag + ' .director-desc');
-                bioContainer.html(director.bio);
-                img = $(idTag + ' .director-pic');
-                img.attr('src', director.image);
-                favouriteMovie = $(idTag + ' .director-movie-inner');
-                favouriteMovie.html(director.movie);
+                //console.log(img);
+                img.attr('src', 'images/' + movie.image);
+                
+                chyrate = $(idTag + ' .chyrate');
+                chyrate.html(movie.chyrate);
+                dateContainer = $(idTag + ' .releaseDate');
+                dateContainer.html(movie.releaseDate);
             }
             movieNum++;
            
@@ -47,6 +42,6 @@ function getAllDirectors() {
     request.send();
 }
 
-function setupDirectorsOnLoad() {
-    getAllDirectors();
+function setupReviewsOnLoad() {
+    getAllReviews();
 }
